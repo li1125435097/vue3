dec448385f6463c3bb24e98e3bcba213240b75f6
“lijinke” <lijinke@xiaoyun.com>
2023-03-15 11:07:47 +0800

    添加博客页面

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref,computed } from 'vue';
+
+// import List from './components/List.vue'
+let list = ref([
+  {
+    id: 1,
+    name: 'John',
+    age: 20,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    id: 2,
+    name: 'Jim',
+    age: 30,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    id: 3,
+    name: 'Joe',
+    age: 28,
+    address: 'Sidney No. 1 Lake Park'
+  }
+])
+
+
+const total = computed(()=>{ return list.value.length })
+
+function handle(e) {
+  let formdata = new FormData(e.target)
+  let [name, age, address] = [formdata.get('name'), formdata.get('age'), formdata.get('address')]
+  list.value.push({id:list.value.pop.id++,name,age,address})
+  // console.log(name, age, address, list.value.data)
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    
+    <div>个数：{{ total }}</div>
+    <form action="" @submit.prevent="handle">
+      名字：<input type="text" name="name" placeholder="请输入名称">
+      年龄：<input type="text" name="age" placeholder="请输入年龄">
+      地址：<input type="text" name="address" placeholder="请输入地址">
+      &emsp;<button>提交</button>
+    </form>
   </main>
 </template>
 
